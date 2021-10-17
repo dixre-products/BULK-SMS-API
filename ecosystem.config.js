0ffec91d09d89dda54931b317ef381b0c10d3b97
Solex <solomonyunana95@gmail.com>
2021-10-17 03:30:17 +0100

    fixed start script for pm2 on production

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name: 'Auth Service API',
-    script: './build/server.js',
+    script: './build/src/server.js',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     args: 'one two',
