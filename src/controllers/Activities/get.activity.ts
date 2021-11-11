c6d91dea6ff864e8603dc9976afdd70d75ae0868
Solex <solomonyunana95@gmail.com>
2021-11-11 12:20:13 +0100

    testin acivities route

--- src/controllers/Activities/get.activity.ts
+++ src/controllers/Activities/get.activity.ts
@@ -1,36 +1,36 @@
 import { Request, Response } from 'express';
 import { ProcessingGetRequestSuccess } from '../../RequestStatus/status';
-import models from '../../models';
-import { getQuery } from '../../utills/utills';
+// import models from '../../models';
+// import { getQuery } from '../../utills/utills';
 
 export default async function AllActivity(
   req: Request,
   res: Response,
 ) {
-  const requestParams = req.query as any;
-  console.log(requestParams);
-  const { paginationConfig, paginationQuery } = getQuery(
-    requestParams,
-    {
-      uid: '',
-      agency: 'group',
-      roles: 'roleId',
-      userType: 'userType',
-    },
-  );
+  const requestParams = req.query as any; // eslint-disable-line
+  // console.log(requestParams);
+  // const { paginationConfig, paginationQuery } = getQuery(
+  //   requestParams,
+  //   {
+  //     uid: '',
+  //     agency: 'group',
+  //     roles: 'roleId',
+  //     userType: 'userType',
+  //   },
+  // );
 
-  const doc = await models.Activities.paginate(
-    { ...paginationQuery },
-    {
-      ...paginationConfig,
-      populate: 'user admin',
-      sort: { date: -1 },
-    },
-  );
+  // const doc = await models.Activities.paginate(
+  //   { ...paginationQuery },
+  //   {
+  //     ...paginationConfig,
+  //     populate: 'user admin',
+  //     sort: { date: -1 },
+  //   },
+  // );
 
   return ProcessingGetRequestSuccess(res, {
-    payload: doc.docs,
-    totalDoc: doc.totalDocs,
-    totalPages: doc.totalPages,
+    payload: [],
+    totalDoc: 0,
+    totalPages: 1,
   });
 }
